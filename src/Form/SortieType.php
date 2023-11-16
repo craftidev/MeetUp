e3eac79fd460292e508c3e7d49a72e0583970ed5
Noémie Lameyse <noemie.lameyse2023@campus-eni.fr>
2023-11-16 10:46:06 +0100

    Corrections prof

---
+++ src/Form/SortieType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use phpDocumentor\Reflection\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dateHeureDebut')
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie', null, ['required' => false])
+            ->add('lieu')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
