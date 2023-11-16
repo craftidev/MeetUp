ff7251a08237754b60fe9db9566a742070330f12
Noémie Lameyse <noemie.lameyse2023@campus-eni.fr>
2023-11-16 14:16:52 +0100

    Corrections prof

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -4,8 +4,14 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +20,13 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie', null, ['required' => false])
-            ->add('lieu')
+            ->add('nom', TextType::class)
+            ->add('dateHeureDebut', DateTimeType::class)
+            ->add('duree', IntegerType::class)
+            ->add('dateLimiteInscription', DateType::class)
+            ->add('nbInscriptionsMax', IntegerType::class)
+            ->add('infosSortie', TextareaType::class,null, ['required' => false])
+            ->add('lieu', TextType::class)
         ;
     }
 
