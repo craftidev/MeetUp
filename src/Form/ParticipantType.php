0728174d009fae48e3b6b001c075af203e041a92
craftidev <ivann.codev@gmail.com>
2023-11-23 11:03:39 +0100

    generated form sf

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('telephone', PhoneType::class)
+            ->add('mail', MailType::class)
+            ->add('pseudo')
+            ->add('motPasse', PasswordType::class)
+            ->add('administrateur')
+            ->add('campus', EntityType::class, ['class' => 'Campus'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
